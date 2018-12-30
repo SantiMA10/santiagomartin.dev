4b05223c60f3b75cee40856dd91707baa48082e1
Santi <santiagomartinagra@gmail.com>
2018-12-30 12:29:59 +0100

    feat(MySideProjects): new component

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <main class="flex items-center flex-col">
     <AboutMe class="mt-10"/>
     <MyTalks class="mt-10"/>
+    <MySideProjects class="mt-10"/>
   </main>
 </template>
 
 <script>
 import AboutMe from '~/components/AboutMe'
 import MyTalks from '~/components/MyTalks'
+import MySideProjects from '~/components/MySideProjects'
 
 export default {
   components: {
     AboutMe,
-    MyTalks
+    MyTalks,
+    MySideProjects
   }
 }
 </script>
