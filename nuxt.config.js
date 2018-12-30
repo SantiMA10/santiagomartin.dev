a6fb53dd7d77a66045922dcb5fddc82a37eb1e82
Santi <santiagomartinagra@gmail.com>
2018-12-30 16:45:50 +0100

    perf(nux.config): font preload

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,8 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
-        rel: 'stylesheet',
+        rel: 'preload',
+        as: 'stylesheet',
         href: 'https://fonts.googleapis.com/css?family=Source+Code+Pro'
       }
     ]
