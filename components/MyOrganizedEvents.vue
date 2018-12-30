856664394ea957e69aa3b54086bb9df58715d2d6
Santi <santiagomartinagra@gmail.com>
2018-12-30 12:36:30 +0100

    feat(MyOrganizedEvents): new component

---
+++ components/MyOrganizedEvents.vue
@@ -0,0 +1,54 @@
+<template>
+  <section
+    id="events"
+    class="w-3/4">
+    <h1 class="mb-2">
+      Organized events
+    </h1>
+    <ul class="list-reset">
+      <li
+        v-for="{time, event} in events"
+        :key="time"
+        class="ml-5"
+      >
+        🗓 {{ time | formatTime }} - <MyLink :link="event"/>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import MyLink from './MyLink'
+import { DateTime } from 'luxon'
+
+export default {
+  components: {
+    MyLink
+  },
+  filters: {
+    formatTime(time) {
+      return DateTime.fromMillis(time).toFormat('MMM yyyy')
+    }
+  },
+  data() {
+    return {
+      events: [
+        {
+          time: 1543654800000,
+          event: {
+            name: 'DevFest Asturias 2018',
+            url: 'https://www.meetup.com/GDG-Asturias/events/254241974/'
+          }
+        },
+        {
+          time: 1523692800000,
+          event: {
+            name: 'Women Techmakers Asturias 2018',
+            url: 'https://www.meetup.com/GDG-Asturias/events/246868757/'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <AboutMe class="mt-10"/>
     <MyTalks class="mt-10"/>
     <MySideProjects class="mt-10"/>
+    <MyOrganizedEvents class="mt-10"/>
   </main>
 </template>
 
@@ -10,12 +11,14 @@
 import AboutMe from '~/components/AboutMe'
 import MyTalks from '~/components/MyTalks'
 import MySideProjects from '~/components/MySideProjects'
+import MyOrganizedEvents from '~/components/MyOrganizedEvents'
 
 export default {
   components: {
     AboutMe,
     MyTalks,
-    MySideProjects
+    MySideProjects,
+    MyOrganizedEvents
   }
 }
 </script>
