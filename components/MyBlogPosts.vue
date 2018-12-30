310f5e3b6d9b041cb55f245530fbad0b4498dda7
Santi <santiagomartinagra@gmail.com>
2018-12-30 17:40:05 +0100

    feat(MyBlogPosts): new component

---
+++ components/MyBlogPosts.vue
@@ -0,0 +1,58 @@
+<template>
+  <section
+    id="talks"
+    class="w-3/4">
+    <h1 class="mb-4">
+      My blog posts
+    </h1>
+    <ul class="list-reset">
+      <li
+        v-for="({title, url, time, key}) in lastest"
+        :key="key"
+        class="sm:ml-5 mb-2"
+      >
+        📝 {{ time | formatTime }} - <a :href="url">{{ title }}</a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import MyLink from './MyLink'
+import { DateTime } from 'luxon'
+
+export default {
+  components: {
+    MyLink
+  },
+  filters: {
+    formatTime(time) {
+      return DateTime.fromMillis(time).toFormat('MMM yyyy')
+    }
+  },
+  props: {
+    posts: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    lastest() {
+      return (
+        Object.keys(this.posts)
+          .map(key => {
+            const { title, firstPublishedAt, uniqueSlug } = this.posts[key]
+            return {
+              time: firstPublishedAt,
+              url: `https://medium.com/@Santi/${uniqueSlug}`,
+              key,
+              title
+            }
+          })
+          // get the last 3 post
+          .slice(0, 3)
+      )
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <main class="flex items-center flex-col">
     <AboutMe class="mt-10"/>
+    <MyBlogPosts
+      :posts="posts"
+      class="mt-5"/>
     <MyTalks class="mt-5"/>
     <MySideProjects class="mt-5"/>
     <MyOrganizedEvents class="mt-5"/>
@@ -11,6 +14,7 @@
 <script>
 import AboutMe from '~/components/AboutMe'
 import MyTalks from '~/components/MyTalks'
+import MyBlogPosts from '~/components/MyBlogPosts'
 import MySideProjects from '~/components/MySideProjects'
 import MyOrganizedEvents from '~/components/MyOrganizedEvents'
 import ContactMe from '~/components/ContactMe'
@@ -19,9 +23,17 @@ export default {
   components: {
     AboutMe,
     MyTalks,
+    MyBlogPosts,
     MySideProjects,
     MyOrganizedEvents,
     ContactMe
+  },
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get(
+      'https://medium.com/@santi/latest?format=json'
+    )
+    const { payload } = JSON.parse(data.replace('])}while(1);</x>', ''))
+    return { posts: payload.references.Post }
   }
 }
 </script>
