4081cb04b06c04acdbdcd3a1eda425051a5f69aa
Santi <santiagomartinagra@gmail.com>
2018-12-30 11:29:21 +0100

    feat(AboutMe): New component

---
+++ components/AboutMe.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="w-3/4">
+    <h1 class="mb-2">
+      Hi 👋🏻, I'm Santi
+    </h1>
+    <p>
+      I'm a software engineer who lives in <a href="https://www.google.es/maps/place/Gij%C3%B3n,+Asturias/@43.5314853,-5.7034739,13z/data=!3m1!4b1!4m5!3m4!1s0xd367c66cbf494bf:0xbd06d207048d3536!8m2!3d43.5322015!4d-5.6611195">Gijón, Asturias</a>.
+    </p>
+    <p>
+      Actually I work for a company called <a href="https://simbiosys.es">Simbiosys</a>, co-organize <a href="https://www.meetup.com/es-ES/GDG-Asturias">GDG Asturias</a> and I have fun coding some <a href="#sideprojects">side projects</a>.
+    </p>
+    <p>
+      I also play basketball 🏀 with some friends on every time I can.
+    </p>
+  </section>
+</template>
