93a9fc73cd595ed2ce1787b6ff2db26c7366f0ca
Santiago Martín <santiagomartinagra@gmail.com>
2019-04-20 11:45:21 +0200

    feat: new cron cloud function to deploy website every 24h

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import axios from 'axios'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const scheduledFunctionCrontab = functions.pubsub
+  .schedule('every 24 hours')
+  .onRun(() => {
+    return axios.post(functions.config().netlify.webhook)
+  })
