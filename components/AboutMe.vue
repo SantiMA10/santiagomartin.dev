635d737d53325fd9f917c27ac58d822e0d9f44d3
Santiago Martín <santiagomartinagra@gmail.com>
2020-03-13 16:29:20 +0100

    fix: missing point in home

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,24 +1,22 @@
 <template>
   <section id="aboutme" class="w-3/4">
     <h1 class="mb-4">
-      Hi 👋🏻, I'm Santi
+      Hi 👋🏻, I'm Santi.
     </h1>
     <p class="mb-2">
       I'm a software engineer who lives in
       <a
         target="_blank"
         href="https://www.google.es/maps/place/Gij%C3%B3n,+Asturias/@43.5314853,-5.7034739,13z/data=!3m1!4b1!4m5!3m4!1s0xd367c66cbf494bf:0xbd06d207048d3536!8m2!3d43.5322015!4d-5.6611195"
-        >Gijón, Asturias</a
-      >.
+        >Gijón, Asturias</a>.
     </p>
     <p class="mb-2">
       Currently I work for a company called
       <a target="_blank" href="https://privacycloud.com">Privacy Cloud</a>,
       co-organize
-      <a target="_blank" href="https://www.meetup.com/es-ES/GDG-Asturias"
-        >GDG Asturias</a
-      >
-      and I have fun coding some <a href="#sideprojects">side projects</a>.
+      <a target="_blank" href="https://www.meetup.com/es-ES/GDG-Asturias">GDG Asturias</a>
+      and I have fun coding some
+      <a href="#sideprojects">side projects</a>.
     </p>
     <p class="mb-2">
       I also play basketball 🏀 with some friends on every time I can.
