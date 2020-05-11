301999e6cd9525044360a678ac791a9494057901
Santiago Martín <santiagomartinagra@gmail.com>
2020-05-11 20:44:49 +0200

    chore: remove unneeded 'api/post'

--- src/pages/api/post.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { getPostFromMedium } from "../../services/getPostFromMedium";
-
-export default async (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ posts: await getPostFromMedium() });
-};
