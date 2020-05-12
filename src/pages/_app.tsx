28506d1163fd98d52466a9282141a87f64e18817
Santiago Martín <santiagomartinagra@gmail.com>
2020-05-12 21:57:05 +0200

    feat: add firebase analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,14 @@ import "../css/tailwind.css";
 import "../css/global.css";
 import "../../node_modules/react-typist/dist/Typist.css";
 import { Footer } from "../components/Footer";
+import { useEffect } from "react";
+import { initFirebase } from "../services/initFirebase";
 
 export function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    initFirebase();
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
---
+++ src/services/initFirebase.ts
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/performance";
+
+export const initFirebase = () => {
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp({
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    });
+  }
+  firebase.analytics();
+  firebase.performance();
+};
