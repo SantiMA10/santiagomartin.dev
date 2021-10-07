fd1cefb7e16a755188c084e7687a63b1995b725c
Santiago Martín Agra <santiagomartinagra@gmail.com>
2021-10-07 19:34:13 +0200

    feat: configure NEXT.js redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+
+	async redirects() {
+		return [
+			{
+				source: '/twitter',
+				destination: 'https://twitter.com/SantiMA10b',
+				permanent: false,
+			},
+			{
+				source: '/twitch',
+				destination: 'https://twitch.tv/SantiMA10',
+				permanent: false,
+			},
+			{
+				source: '/github',
+				destination: 'https://github.com/SantiMA10',
+				permanent: false,
+			},
+			{
+				source: '/streamdeck-devops',
+				destination: 'streamdeck://plugin/install/dev.santiagomartin.devops',
+				permanent: false,
+			},
+		];
+	},
+};
