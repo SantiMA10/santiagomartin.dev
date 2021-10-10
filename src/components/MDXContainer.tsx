a98171e3bdf917fdc8e719c3d9e458ac59ec0d57
Santiago Martín Agra <santiagomartinagra@gmail.com>
2021-10-10 19:26:21 +0200

    feat: parse inline styles

--- src/components/MDXContainer.tsx
+++ src/components/MDXContainer.tsx
@@ -1,15 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
-import { AnchorHTMLAttributes, ComponentProps, CSSProperties, DetailedHTMLProps } from 'react';
+import { AnchorHTMLAttributes, ComponentProps, DetailedHTMLProps } from 'react';
 
 interface Props {
 	source: MDXRemoteSerializeResult<Record<string, unknown>>;
 }
 
 const components = {
-	img: (props: ComponentProps<typeof Image> & { style?: CSSProperties }) => {
+	img: (props: ComponentProps<typeof Image> & { style?: string }) => {
 		if (props.src.toString().includes('http') && typeof props.src === 'string') {
+			const styles = props.style?.split(';').reduce((all, style) => {
+				const [name, value] = style.split(':');
+
+				return {
+					...all,
+					[name]: value,
+				};
+			}, {});
+
 			return (
 				// eslint-disable-next-line @next/next/no-img-element
 				<img
@@ -17,7 +26,7 @@ const components = {
 					alt={props?.alt}
 					width={props?.width}
 					height={props?.height}
-					style={props?.style}
+					style={styles}
 				/>
 			);
 		}
