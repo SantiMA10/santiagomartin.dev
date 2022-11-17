da7078604676150aa1b11ab56a45b98b28b3a5ed
Santiago Martín Agra <santiagomartinagra@gmail.com>
2022-11-17 20:41:06 +0100

    feat: add rss

---
+++ bin/rss.ts
@@ -0,0 +1,38 @@
+import { Feed } from 'feed';
+import { writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { getDocuments } from 'outstatic/server';
+
+(async function run() {
+	const author = {
+		name: 'Santiago Martín Agra',
+		email: 'web@santiagomartin.dev',
+		link: 'https://santiagomartin.dev',
+	};
+
+	const feed = new Feed({
+		title: '/blog - Santiago Martín',
+		id: 'https://santiagomartin.dev/',
+		link: 'https://santiagomartin.dev/',
+		language: 'es',
+		image: 'https://santiagomartin.dev/favicon.ico',
+		favicon: 'https://santiagomartin.dev/favicon.ico',
+		copyright: 'All rights reserved 2022, Santiago Martin',
+		author,
+	});
+
+	const posts = getDocuments('posts', ['title', 'publishedAt', 'slug', 'description', 'content']);
+	posts.forEach((post) => {
+		feed.addItem({
+			title: post.title,
+			id: post.slug,
+			link: `https://santiagomartin.dev/blog/${post.slug}`,
+			description: post.description,
+			content: post.content,
+			author: [author],
+			date: new Date(post.publishedAt),
+		});
+	});
+
+	await writeFile(join(__dirname, '../public/rss.xml'), feed.atom1());
+})();
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,22 +13,32 @@ export default function NavBar(): JSX.Element {
 	return (
 		<div className="flex justify-between mb-4">
 			<div className="flex">
-				<NavItem href="/">🏡 Home</NavItem>
-				<NavItem href="/blog" subPaths>
-					✍️ /blog
+				<NavItem href="/">
+					🏡 <span className="hidden sm:inline-block">Home</span>
 				</NavItem>
-				<NavItem href="/now">📅 /now</NavItem>
-				{/* <NavItem href="/uses">💾 /uses</NavItem>
-				<NavItem href="/open">🔓 /open</NavItem> */}
+				<NavItem href="/blog" subPaths>
+					✍️ <span className="hidden sm:inline-block">/blog</span>
+				</NavItem>
+				<NavItem href="/now">
+					📅 <span className="hidden sm:inline-block">/now</span>
+				</NavItem>
 			</div>
-			<button
-				aria-label="Toggle Dark Mode"
-				type="button"
-				className="flex-end w-10 h-10 p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 flex align-center justify-center hover:ring-2 ring-gray-300 transition-all"
-				onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
-			>
-				{mounted && resolvedTheme === 'dark' ? <span>☀️</span> : <span>🌙</span>}
-			</button>
+			<div className="flex">
+				<button
+					aria-label="Toggle Dark Mode"
+					type="button"
+					className="flex-end w-10 h-10 p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 flex align-center justify-center hover:ring-2 ring-gray-300 transition-all"
+					onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+				>
+					{mounted && resolvedTheme === 'dark' ? <span>☀️</span> : <span>🌙</span>}
+				</button>
+				<a
+					className="flex-end w-30 h-10 p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 hover:ring-2 ring-gray-300"
+					href="/rss.xml"
+				>
+					<span className="hidden sm:inline-block">🔔</span> RSS
+				</a>
+			</div>
 		</div>
 	);
 }
