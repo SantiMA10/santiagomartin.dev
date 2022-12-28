542f34e952ad8f1ac925a4c658bb6099427c2095
Santiago Martín Agra <santiagomartinagra@gmail.com>
2022-12-28 17:07:41 +0100

    feat: vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/global.css';
 
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	return (
-		<ThemeProvider attribute="class">
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<>
+			<ThemeProvider attribute="class">
+				<Component {...pageProps} />
+			</ThemeProvider>
+
+			<Analytics />
+		</>
 	);
 }
 
